841e723228606f550a584d648bb74fc87f7a9af5
luuk <klaverluuk@gmail.com>
2018-01-21 19:24:14 +0100

    precommit hook

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  scripts: {
-    extensions: "*/.js",
-    parser: "flow",
-    printWidth: 100,
-    tabWidth: 4
-  },
+    scripts: {
+        write: "**/*.js",
+        parser: "flow",
+        printWidth: 100,
+        tabWidth: 4
+    },
 
-  styles: {
-    extensions: "*/.scss",
-    parser: "scss",
-    printWidth: 100,
-    tabwidth: 1,
-    trailingComma: "all",
-    bracketSpacing: true
-  }
+    styles: {
+        write: "**/*.scss",
+        parser: "scss",
+        printWidth: 100,
+        tabWidth: 1,
+        trailingComma: "all",
+        bracketSpacing: true
+    }
 };
--- build/precommit.js
+++ build/precommit.js
@@ -1,31 +1,26 @@
 "use strict";
 const fs = require("fs");
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
 const ora = require("ora");
-const spinner = ora("running precommit hook...");
+const chalk = require("chalk");
 
 const config = require("../.prettierrc");
 
-spinner.start();
+console.log(chalk.blue("running precommit hook..."));
 
 Object.keys(config).map(key => {
-  const { extensions, ...rest } = config[key];
-  exec(
-    "prettier '**/*.js' --config .prettierrc.js --write {{changed}} ",
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    }
-  );
+    const child = spawn("prettier", formatConfig(config[key]));
 
-  console.log("tst", rest);
+    child.stdout.on("data", output => console.log(chalk.green(output)));
+
+    child.on("exit", () => console.log(chalk.red(`finished formatting ${key}`)));
 });
-//
-// const text = fs.readFileSync("src/components/HelloWorld.vue", "utf8");
-// console.log(text);
-// const input = prettier.resolveConfig.sync(".prettierrc.json")
-// const formatted = prettier.format('**/*.js', input);
+
+function formatConfig(config) {
+    return Object.keys(config)
+        .map(value => {
+            let formattedValue = value.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
+            return [`--${formattedValue}`, config[value]];
+        })
+        .reduce((prev, curr) => prev.concat(curr));
+}
