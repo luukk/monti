3aabb9a6b5967402cbf51777e01e4944a50b0d62
luuk <klaverluuk@gmail.com>
2018-02-12 18:04:01 +0100

    improved precommit hook output

--- build/precommit.js
+++ build/precommit.js
@@ -3,22 +3,24 @@
 const { readdirSync, readFileSync, writeFileSync, lstatSync } = require("fs");
 const path = require("path");
 const prettier = require("prettier");
-const utils = require('./utils');
+const utils = require("./utils");
 const config = require("../.prettierrc");
 
 utils.$log.warn("running precommit hook...");
 
 const formatFiles = ({ write, extensions, options }) =>
-    findfiles(write).map(filePath => {
-        if (!checkExtension(filePath, extensions)) return;
+    findfiles(write)
+        .map(filePath => {
+            if (!checkExtension(filePath, extensions)) return;
 
-        const file = readFileSync(filePath, { encoding: "utf-8" });
-        const formattedFile = prettier.format(file, options);
+            const file = readFileSync(filePath, { encoding: "utf-8" });
+            const formattedFile = prettier.format(file, options);
 
-        writeFileSync(filePath, formattedFile, { encoding: "utf-8", flag: "w" });
+            writeFileSync(filePath, formattedFile, { encoding: "utf-8", flag: "w" });
 
-        return filePath
-    });
+            return filePath;
+        })
+        .filter(Boolean);
 
 const findfiles = write => {
     if (!lstatSync(write).isDirectory()) return write;
@@ -35,4 +37,7 @@ const checkExtension = (filename, extensions) =>
         })
         .includes(true);
 
-Object.keys(config).map(key => utils.$log.info(formatFiles(config[key])));
+Object.keys(config).map(key => {
+    const formattedFiles = formatFiles(config[key]);
+    formattedFiles.map(formattedFile => utils.$log.info(formattedFile));
+});
